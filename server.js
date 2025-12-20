534cf635d0d5d9c969e65981a291b8105cf26ec3
.wattsonshusband <67444302+wattsonshusband@users.noreply.github.com>
2025-12-20 21:03:36 +0000

    Update server.js

--- server.js
+++ server.js
@@ -56,6 +56,8 @@ app.get("/callback", async (req, res) => {
  `);
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
  console.log("Auth server running on port 3000");
 });
+
