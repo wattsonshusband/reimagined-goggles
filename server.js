8bcd43f9e020fa81dbe98b6690879d06d8d40680
.wattsonshusband <67444302+wattsonshusband@users.noreply.github.com>
2025-12-20 21:33:16 +0000

    Fix formatting issues in server.js

--- server.js
+++ server.js
@@ -41,13 +41,7 @@ app.get("/callback", async (req, res) => {
   {
    headers: {
     "Content-Type": "application/x-www-form-urlencoded",
-    "Authorization":
-     "Basic " +
-     Buffer.from(
-      process.env.SPOTIFY_CLIENT_ID +
-       ":" +
-       process.env.SPOTIFY_CLIENT_SECRET
-     ).toString("base64")
+    "Authorization": "Basic " + Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString("base64"),
    }
   }
  );
