1838ad0107f3d0fdf64455df3d4f768c964d5c04
.wattsonshusband <deox.indura.xyz@gmail.com>
2025-12-27 22:55:16 +0000

    Enhance Redis connection with event listeners
    Added event listeners for Redis connection and error handling.

--- redis.js
+++ redis.js
@@ -1,4 +1,7 @@
 const { Redis } = require('ioredis');
 const redis = new Redis(process.env.REDIS_URL);
 
+redis.on("connect", () => console.log("Redis connected"));
+redis.on("error", (err) => console.error("Redis error", err));
+
 module.exports = redis;
